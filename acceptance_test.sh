b9bbc4fd42e224bcac429c1e567841ea9e025d3f
fulvio <fulviocaruna@gmail.com>
2022-09-05 11:43:11 -0400

    Add Dynamic UAT Support

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-test $(curl 172.17.0.1:8765/sum?a=1\&b=2) -eq 3
-test $(curl 172.17.0.1:8765/sum?a=1\&b=3) -eq 4
-test $(curl 172.17.0.1:8765/sum?a=1\&b=4) -eq 5
+set -x
+
+NODE_IP=$(kubectl --kubeconfig=/config get nodes -o jsonpath={.items[1].status.addresses[?\(@.type==\"InternalIP\"\)].address})
+NODE_PORT=$(kubectl --kubeconfig=/config get svc calculator-service -o=jsonpath='{.spec.ports[0].nodePort}')
+
+test $(curl ${NODE_IP}:${NODE_PORT}/sum?a=1\&b=2) -eq 3
+test $(curl ${NODE_IP}:${NODE_PORT}/sum?a=1\&b=2) -eq 3
+test $(curl ${NODE_IP}:${NODE_PORT}/sum?a=1\&b=2) -eq 3
